d484aceda9053076b29684e1554ca6dd147f85f5
jorabek710ly <jorabekabdurashidov555@gmail.com>
2025-05-06 06:53:13 +0500

    push

--- js/add.js
+++ js/add.js
@@ -1,32 +1,4 @@
-function checkToken() {
-    let token = localStorage.getItem("access_token");
-  
-    fetch("https://dummyjson.com/auth/me", {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${token}`
-      }
-    })
-      .then(res => {
-        if (!res.ok) {
-          throw new Error("Token xato");
-        }
-      })
-      .catch(err => {
-        localStorage.removeItem("access_token");
-        location.replace("/pages/login.html");
-      });
-  }
-  
-  window.onload = () => {
-    checkToken();
-  };
 
-
-
-
-  // ==========================dark mod==================
-  
   const toggle = document.getElementById('theme-toggle');
   const body = document.body;
   const sidebar = document.querySelector('.sidebar');
@@ -58,4 +30,4 @@ function checkToken() {
       localStorage.setItem('darkMode', 'disabled');
     }
   });
-  
+
